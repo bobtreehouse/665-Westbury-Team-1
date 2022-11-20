10dc0b3d184f1b1e50e665a98125828cca376e30
Bob Trieste <48025886+bobtreehouse@users.noreply.github.com>
2022-11-20 07:51:37 -0500

    Fix Fetch Data Component

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { GoogleSheetsDbService } from 'ng-google-sheets-db';
+
 
 import { environment } from 'src/environments/environment';
 import { State, gasAttributesMapping } from './fetch-data.model';
@@ -12,13 +11,11 @@ import { State, gasAttributesMapping } from './fetch-data.model';
   styleUrls: ['./fetch-data.component.css']
 })
 export class FetchDataComponent implements OnInit {
-  states$!: Observable<State[]>;
 
-  constructor(private googleSheetsDbService: GoogleSheetsDbService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.states$ = this.googleSheetsDbService.getActive<State>(
-      environment.states.spreadsheetId, environment.states.worksheetName, gasAttributesMapping, 'Active');
   }
+
 
 }
